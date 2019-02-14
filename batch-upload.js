54a464a3de196e994c9a5143444f465a53244221
Indrajeet <indrajeet1091@gmail.com>
2019-02-14 22:00:20 +1100

    Read CSV file from url using csvtojson stream

--- batch-upload.js
+++ batch-upload.js
@@ -1,4 +1,6 @@
 const pg= require('pg');
+const request= require('request');
+const csv= require('csvtojson');
 
 const config={
     user:"postgres",
@@ -9,3 +11,13 @@ const config={
 }
 
 const pgPool = new pg.Pool(config);
+
+const url = "https://s3-ap-southeast-2.amazonaws.com/testcsvindra/orderData.csv";
+
+csv().fromStream(request.get(url))
+.subscribe((orderRedcord)=>{
+ //some logic after steaming
+})
+.on("done",()=>{
+//some logic after done event
+})
