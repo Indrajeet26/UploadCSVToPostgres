395b1c6fd262f1bdc70a65ee32e9a412d5799772
Indrajeet <indrajeet1091@gmail.com>
2019-02-14 22:15:07 +1100

    Insert each row into postgres aftre reading from stream

--- batch-upload.js
+++ batch-upload.js
@@ -14,9 +14,21 @@ const pgPool = new pg.Pool(config);
 
 const url = "https://s3-ap-southeast-2.amazonaws.com/testcsvindra/orderData.csv";
 
+
+const populateOrderData = (pgPool, orderRedcord)=>{
+    pgPoool.query("INSERT INTO public.order (orderid, customerid, item, quantity)" +
+                " VALUES ($1, $2, $3, $4) RETURNING *", 
+            [       orderRedcord.orderid,
+                    orderRedcord.customerid,
+                    orderRedcord.item,
+                    orderRedcord.quantity
+
+                ])
+
+}
 csv().fromStream(request.get(url))
 .subscribe((orderRedcord)=>{
- //some logic after steaming
+   populateOrderData(pgPool, orderRedcord);
 })
 .on("done",()=>{
 //some logic after done event
