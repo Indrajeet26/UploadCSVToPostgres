d33afd00ae687c92b8d053e3485155faf7d4bdfc
Indrajeet <indrajeet1091@gmail.com>
2019-02-15 15:44:07 +1100

    Format Code and Test

--- src/batch-upload.js
+++ src/batch-upload.js
@@ -1,49 +1,47 @@
+const format = require("pg-format");
 
-const format=require('pg-format');
+const DEFAULT_MAX_BATCH_SIZE = 1000;
 
-const DEFAULT_MAX_BATCH_SIZE =1000;
+const QUERY_PREFIX =
+  "INSERT INTO public.order (orderid, customerid, item, quantity) SELECT i.orderid,i.customerid, i.item, i.quantity FROM(" +
+  " VALUES %L ) AS i (orderid, customerid, item, quantity)" +
+  " WHERE exists (SELECT customerid from public.customer WHERE customer.customerid=i.customerid)";
 
-const QUERY_PREFIX = "INSERT INTO public.order (orderid, customerid, item, quantity) SELECT i.orderid,i.customerid, i.item, i.quantity FROM(" +
-" VALUES %L ) AS i (orderid, customerid, item, quantity)" + 
-" WHERE exists (SELECT customerid from public.customer WHERE customer.customerid=i.customerid)";
+/* eslint no-underscore-dangle: 0 */
+const _insertRecord = (
+  queries,
+  executeBatch,
+  batchSize
+) => async orderRedcord => {
+  queries.push(Object.values(orderRedcord));
+  if (queries.length === batchSize) {
+    const queriesBatchTemp = queries.slice(0, batchSize);
 
-
-const _insertRecord = (queries, executeBatch,batchSize ) => async orderRedcord=>{
-    queries.push(Object.values(orderRedcord));
-    if(queries.length===batchSize)
-    {
-        const queriesBatchTemp= queries.slice(0,batchSize);
-            
-            await executeBatch(queriesBatchTemp); 
-            queries=queries.slice(batchSize)
-             
-    }
+    await executeBatch(queriesBatchTemp);
+    queries = queries.slice(batchSize);
+  }
   return queries;
 };
 
-
 const _executeBatch = dbService => (queries = []) => {
-    if(queries.length===0) return;
+  if (queries.length === 0) return;
 
-    const queryString = format(QUERY_PREFIX,queries);
-    return dbService.query(queryString);
-}
+  const queryString = format(QUERY_PREFIX, queries);
+  return dbService.query(queryString);
+};
 
-
-const batchJob =(converter,dbService,inputBatchSize)=>{
-    let queries;
+const batchJob = (converter, dbService, inputBatchSize) => {
+  let queries;
   const batchSize = inputBatchSize || DEFAULT_MAX_BATCH_SIZE;
-    const executeBatch = _executeBatch(dbService);
-    const insertRecord = _insertRecord([],executeBatch,batchSize);
-    converter
-.subscribe(async orderRedcord=>{
-queries = await insertRecord(orderRedcord);
-})
-.on("done", async ()=>{
-    await executeBatch(queries);
-});
-}
+  const executeBatch = _executeBatch(dbService);
+  const insertRecord = _insertRecord([], executeBatch, batchSize);
+  converter
+    .subscribe(async orderRedcord => {
+      queries = await insertRecord(orderRedcord);
+    })
+    .on("done", async () => {
+      await executeBatch(queries);
+    });
+};
 
-module.exports = {batchJob,
-_executeBatch,
-_insertRecord};
+module.exports = { batchJob, _executeBatch, _insertRecord };
--- src/batch-upload.test.js
+++ src/batch-upload.test.js
@@ -1,102 +1,98 @@
 const chai = require("chai");
-const expect =chai.expect;
 
-describe("_executeBatch test cases", () =>{
-    const {_executeBatch } = require("./batch-upload");
+const expect = chai.expect;
 
-    it("Should not execute queries if queries batch is empty", ()=>{
-        let counter =1;
-        const dbService ={
-            query:()=>{
-                counter=counter+1;
-            }
-        };
- 
-        const executeBatch = _executeBatch(dbService);
-        executeBatch([]);
-        expect(counter).to.be.equal(1);
+describe("_executeBatch test cases", () => {
+  const { _executeBatch } = require("./batch-upload");
 
-    });
+  it("Should not execute queries if queries batch is empty", () => {
+    let counter = 1;
+    const dbService = {
+      query: () => {
+        counter += 1;
+      }
+    };
 
-    it("Should execute queries if queries batch is not empty", ()=>{
-        let counter =1;
-        const dbService ={
-            query: qs =>{
-                expectedQueryString="INSERT INTO public.order (orderid, customerid, item, quantity) SELECT i.orderid,i.customerid, i.item, i.quantity FROM( VALUES ('0'), ('1') ) AS i (orderid, customerid, item, quantity) WHERE exists (SELECT customerid from public.customer WHERE customer.customerid=i.customerid)"
-                expect(qs).to.be.equal(expectedQueryString);
-                counter=counter+1;
-            }
-        };
- 
-        const executeBatch = _executeBatch(dbService);
-        executeBatch([[0],[1]]);
-        expect(counter).to.be.equal(2);
+    const executeBatch = _executeBatch(dbService);
+    executeBatch([]);
+    expect(counter).to.be.equal(1);
+  });
 
-    })
-}
-);
+  it("Should execute queries if queries batch is not empty", () => {
+    let counter = 1;
+    const dbService = {
+      query: qs => {
+        const expectedQueryString =
+          "INSERT INTO public.order (orderid, customerid, item, quantity) SELECT i.orderid,i.customerid, i.item, i.quantity FROM( VALUES ('0'), ('1') ) AS i (orderid, customerid, item, quantity) WHERE exists (SELECT customerid from public.customer WHERE customer.customerid=i.customerid)";
+        expect(qs).to.be.equal(expectedQueryString);
+        counter += 1;
+      }
+    };
 
-describe("_insertRecord test cases", () =>{
-    const {_insertRecord } = require("./batch-upload");
+    const executeBatch = _executeBatch(dbService);
+    executeBatch([[0], [1]]);
+    expect(counter).to.be.equal(2);
+  });
+});
 
-    it("Should not execute unless max limit is reached", async ()=>{
-        let queries ;
-        let counter = 1;
-         const executeBatch = () => {
-             counter=counter+1;
-         }
-        const batchSize =4;
-        
-        const insertRecord = _insertRecord([],executeBatch,batchSize);
-        for(let i=0;i<batchSize-1;i++){
-            queries = await insertRecord(i);
-        }
-        expect(counter).to.be.equal(1);
-        expect(queries.length).to.be.eq(batchSize-1);
-        
+describe("_insertRecord test cases", () => {
+  const { _insertRecord } = require("./batch-upload");
 
-    });
+  it("Should not execute unless max limit is reached", async () => {
+    let counter = 1;
+    const executeBatch = () => {
+      counter += 1;
+    };
+    const batchSize = 4;
 
-    it("Should execute batch if max limit is reached and clear query batch", async ()=>{
-        let queries ;
-        let counter = 1;
-        const batchSize = 3;
-         const executeBatch = arr => {
-             const expectedArr =[[0],[1],[2]];
-             counter = counter + 1;
-             expect(JSON.stringify(expectedArr)).to.be.equal(JSON.stringify(arr));
-         }
-        
-        const insertRecord = _insertRecord([],executeBatch,batchSize);
+    const insertRecord = _insertRecord([], executeBatch, batchSize);
 
-        for(let i=0;i<batchSize;i++){
-            queries = await insertRecord({i});
-        }
+    const promises = [];
+    for (let i = 0; i < batchSize - 1; i = i + 1) {
+      promises.push(insertRecord(i));
+    }
 
-        expect(counter).to.be.equal(2);
-        expect(queries.length).to.be.eq(0);
-        
+    const queryResponses = await Promise.all(promises);
 
-    });
+    const queries = queryResponses[queryResponses.length - 1];
 
-    it("Should throw error if execute batch fails due to db issue", async ()=>{
-        const errMsg = "Failed to insertRecord";
-        const batchSize = 3;
-         const executeBatch = () => {
-             
-             throw new Error(errMsg);
-         }
-        
-        const insertRecord = _insertRecord([],executeBatch,batchSize);
+    expect(counter).to.be.equal(1);
+    expect(queries.length).to.be.eq(batchSize - 1);
+  });
 
+  it("Should execute batch if max limit is reached and clear query batch", async () => {
+    let queries;
+    let counter = 1;
+    const batchSize = 3;
+    const executeBatch = arr => {
+      const expectedArr = [[0], [1], [2]];
+      counter += 1;
+      expect(JSON.stringify(expectedArr)).to.be.equal(JSON.stringify(arr));
+    };
 
-        try{
-            await insertRecord({});
-        } catch(err){
-            expect(err.message).to.be.equal(errMsg);
-        }
+    const insertRecord = _insertRecord([], executeBatch, batchSize);
 
-        
+    for (let i = 0; i < batchSize; i++) {
+      queries = await insertRecord({ i });
+    }
 
-    });
-})
+    expect(counter).to.be.equal(2);
+    expect(queries.length).to.be.eq(0);
+  });
+
+  it("Should throw error if execute batch fails due to db issue", async () => {
+    const errMsg = "Failed to insertRecord";
+    const batchSize = 3;
+    const executeBatch = () => {
+      throw new Error(errMsg);
+    };
+
+    const insertRecord = _insertRecord([], executeBatch, batchSize);
+
+    try {
+      await insertRecord({});
+    } catch (err) {
+      expect(err.message).to.be.equal(errMsg);
+    }
+  });
+});
